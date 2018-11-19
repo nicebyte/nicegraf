0357fce14dd8883b9ad6b527a1ff2bb1a825e6ba
nicebyte <nicebyte@mailinator.com>
2018-11-18 20:20:57 -0800

    [Metal] Add placeholders for other functions (besides command buffers)

--- include/nicegraf.h
+++ include/nicegraf.h
@@ -1,8 +1,8 @@
 /**
-Copyright © 2018 nicegraf contributors
+Copyright (c) 2018 nicegraf contributors
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the “Software”), to deal in
+this software and associated documentation files (the "Software"), to deal in
 the Software without restriction, including without limitation the rights to
 use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 the Software, and to permit persons to whom the Software is furnished to do so,
@@ -11,7 +11,7 @@ subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -921,12 +921,6 @@ void ngf_set_allocation_callbacks(const ngf_allocation_callbacks *callbacks);
 
 void ngf_debug_message_callback(void *userdata,
                                 void (*callback)(const char*, const void*));
-
-void ngf_insert_log_message(const char *message);
-
-void ngf_begin_debug_group(const char *title);
-
-void ngf_end_debug_group();
 
 /**
  * Create a shader stage from its description.
