db1ed1fd9c7325fa4e4af7ea3723331df69905a0
nicebyte@mailinator.com <nicebyte@nicebyte>
2019-01-26 12:05:02 -0800

    [misc] Add more functionality to dynamic array

--- source/dynamic_array.h
+++ source/dynamic_array.h
@@ -38,18 +38,49 @@ SOFTWARE.
   a.capacity = c; \
 }
 
-#define _NGF_DARRAY_DESTROY(a) (free(a.data))
+#define _NGF_DARRAY_DESTROY(a) if(a.data != NULL) { \
+  free(a.data); \
+  a.data = a.endptr = NULL; \
+}
 
 #define _NGF_DARRAY_APPEND(a, v) { \
   ptrdiff_t cur_size = a.endptr - a.data; \
   if (cur_size >= a.capacity) { \
     a.capacity <<= 1u; \
-    a.data = (decltype(a.data)) realloc(a.data, sizeof(a.data[0]) * a.capacity); \
+    decltype(a.data) tmp = (decltype(a.data)) realloc(a.data, sizeof(a.data[0]) * a.capacity); \
+    assert(tmp != NULL); \
+    a.data = tmp; \
     a.endptr = &a.data[cur_size]; \
   } \
   *(a.endptr++) = v; \
 }
 
 #define _NGF_DARRAY_CLEAR(a) (a.endptr = a.data)
-#define _NGF_DARRAY_SIZE(a) (a.endptr - a.data)
+#define _NGF_DARRAY_SIZE(a) ((uint32_t)(a.endptr - a.data))
 #define _NGF_DARRAY_AT(a, i) (a.data[i])
+
+#define _NGF_DARRAY_POP(a) {\
+  assert(a->data != a->endptr); \
+  --a->endptr; \
+}
+
+#define _NGF_DARRAY_SWAP(a, i, j) { \
+    assert(i < _NGF_DARRAY_SIZE(a) && j < _NGF_DARRAY_SIZE(a)); \
+    if (i != j) { \
+        uint8_t *tmp = alloca(sizeof(a.data[0])); \
+        memcpy(tmp, &a.data[i], sizeof(a.data[i])); \
+        a.data[i] = a.data[j]; \
+        memcpy(&a.data[j], tmp, sizeof(a.data[j])); \
+    } \
+}
+
+#define _NGF_DARRAY_ERASE(a, i) {\
+  assert(a.endptr != a.data); \
+  uint32_t lastidx = _NGF_DARRAY_SIZE(a) - 1u; \
+  if (i < lastidx) _NGF_DARRAY_SWAP(a, i, lastidx); \
+  _NGF_DARRAY_POP(a); \
+}
+
+#define _NGF_DARRAY_EMPTY(a) (a.endptr == a.data)
+
+#define _NGF_DARRAY_BACKPTR(a) (a.endptr - 1)
