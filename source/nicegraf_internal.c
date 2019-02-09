e7495e3f1815d14f7f8e7c0f89b808ad5b9e25f6
nicebyte@mailinator.com <nicebyte@gpfault.net>
2019-02-09 00:33:18 -0800

    [Util] Grow block allocator when space runs out

--- source/nicegraf_internal.c
+++ source/nicegraf_internal.c
@@ -1,28 +1,30 @@
 /**
-Copyright © 2018 nicegraf contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the “Software”), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ * Copyright (c) 2019 nicegraf contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 #include "nicegraf_internal.h"
+#include "dynamic_array.h"
 #include <stdlib.h>
 
+// Default allocation callbacks.
 void* ngf_default_alloc(size_t obj_size, size_t nobjs) {
   return malloc(obj_size * nobjs);
 }
@@ -37,8 +39,7 @@ const ngf_allocation_callbacks NGF_DEFAULT_ALLOC_CB = {
   ngf_default_free
 };
 
-const ngf_allocation_callbacks *NGF_ALLOC_CB =
-    &NGF_DEFAULT_ALLOC_CB;
+const ngf_allocation_callbacks *NGF_ALLOC_CB = &NGF_DEFAULT_ALLOC_CB;
 
 void ngf_set_allocation_callbacks(const ngf_allocation_callbacks *callbacks) {
   if (callbacks == NULL) {
@@ -48,63 +49,91 @@ void ngf_set_allocation_callbacks(const ngf_allocation_callbacks *callbacks) {
   }
 }
 
+/**
+ * The block allocator. doles out memory in fixed-size blocks from a pool.
+ * A block allocator is created with a certain initial capacity. If the block
+ * capacity is exceeded, an additional block pool is allocated.
+ */
+
 typedef struct _ngf_blkalloc_block  _ngf_blkalloc_block;
 
-typedef struct {
-  _ngf_blkalloc_block *next_free;
-  uint32_t marker_and_tag;
+typedef struct _ngf_blkalloc_block_header { // Block metadata.
+ _ngf_blkalloc_block *next_free;      // Points to the next free block in list.
+  uint32_t            marker_and_tag; // Identifies the parent allocator.
 } _ngf_blkalloc_block_header;
 
-struct _ngf_blkalloc_block {
-  _ngf_blkalloc_block_header header;
-  uint8_t data[];
+struct _ngf_blkalloc_block { // The block itself.
+ _ngf_blkalloc_block_header header;
+  uint8_t                   data[];
 };
 
 struct _ngf_block_allocator {
-  uint8_t *chunk;
-  _ngf_blkalloc_block *freelist;
-  uint32_t block_size;
-  uint32_t nblocks;
-  uint32_t tag;
+ _NGF_DARRAY_OF(uint8_t*)  pools;
+ _ngf_blkalloc_block      *freelist;
+  size_t                   block_size;
+  uint32_t                 nblocks;
+  uint32_t                 tag;
 };
 
 static const uint32_t MARKER_MASK = (1u << 31);
 
-_ngf_block_allocator* _ngf_blkalloc_create(uint32_t block_size, uint32_t nblocks) {
+static void _ngf_blkallock_add_pool(_ngf_block_allocator *alloc) {
+ _ngf_blkalloc_block *old_freelist = alloc->freelist;
+  const size_t        pool_size    = alloc->block_size * alloc->nblocks;
+  uint8_t            *pool         = NGF_ALLOCN(uint8_t, pool_size);
+
+  alloc->freelist = (_ngf_blkalloc_block*)pool;
+  for (uint32_t b = 0u; b < alloc->nblocks; ++b) {
+    _ngf_blkalloc_block *blk =
+        (_ngf_blkalloc_block*)(pool + alloc->block_size * b);
+    _ngf_blkalloc_block *next_blk =
+        (_ngf_blkalloc_block*)(pool + alloc->block_size * (b + 1u));
+    blk->header.next_free =
+        (b < alloc->nblocks - 1u) ? next_blk : old_freelist;
+    blk->header.marker_and_tag = (~MARKER_MASK) & alloc->tag;
+  }
+  _NGF_DARRAY_APPEND(alloc->pools, pool);
+}
+
+_ngf_block_allocator* _ngf_blkalloc_create(uint32_t requested_block_size,
+                                           uint32_t nblocks) {
   static NGF_THREADLOCAL uint32_t next_tag = 0u;
   if (next_tag == 0u) {
     uint32_t threadid = (uint32_t)_ngf_cur_thread_id();
     next_tag = (~MARKER_MASK) & (threadid << 16);
   }
+
   _ngf_block_allocator *alloc = NGF_ALLOC(_ngf_block_allocator);
-  const uint32_t total_unaligned_block_size =
-      block_size + sizeof(_ngf_blkalloc_block);
-  const uint32_t total_aligned_block_size =
-      total_unaligned_block_size +
-      (total_unaligned_block_size % sizeof(_ngf_blkalloc_block*));
-  alloc->block_size = total_aligned_block_size;
-  alloc->chunk = NGF_ALLOCN(uint8_t, alloc->block_size * nblocks);
-  alloc->nblocks = nblocks;
-  alloc->freelist = (_ngf_blkalloc_block*)alloc->chunk;
-  for (uint32_t b = 0u; b < nblocks; ++b) {
-    _ngf_blkalloc_block *blk =
-        (_ngf_blkalloc_block*)(alloc->chunk + alloc->block_size * b);
-    _ngf_blkalloc_block *next_blk =
-        (_ngf_blkalloc_block*)(alloc->chunk + alloc->block_size * (b + 1u));
-    blk->header.next_free = (b < nblocks - 1u) ? next_blk : NULL;
-    blk->header.marker_and_tag = (~MARKER_MASK) & next_tag;
-  }
-  alloc->tag = next_tag++;
-  alloc->freelist = (_ngf_blkalloc_block*)alloc->chunk;
+  if (alloc == NULL) { return NULL; }
+
+  const size_t unaligned_block_size =
+      requested_block_size + sizeof(_ngf_blkalloc_block);
+  const size_t aligned_block_size =
+      unaligned_block_size +
+     (unaligned_block_size % sizeof(_ngf_blkalloc_block*));
+
+  alloc->block_size = aligned_block_size;
+  alloc->nblocks    = nblocks;
+  alloc->tag        = next_tag++;
+  _NGF_DARRAY_RESET(alloc->pools, 8u);
+  alloc->freelist = NULL;
+  _ngf_blkallock_add_pool(alloc);
   return alloc;
 }
 
 void _ngf_blkalloc_destroy(_ngf_block_allocator *alloc) {
-  NGF_FREEN(alloc->chunk, alloc->block_size * alloc->nblocks);
+  for (uint32_t i = 0u; i < _NGF_DARRAY_SIZE(alloc->pools); ++i) {
+    uint8_t *pool = _NGF_DARRAY_AT(alloc->pools, i);
+    if (pool) { NGF_FREEN(pool, alloc->block_size * alloc->nblocks); }
+  }
+  _NGF_DARRAY_DESTROY(alloc->pools);
   NGF_FREE(alloc);
 }
 
 void* _ngf_blkalloc_alloc(_ngf_block_allocator *alloc) {
+  if (alloc->freelist) {
+    _ngf_blkallock_add_pool(alloc);
+  }
   _ngf_blkalloc_block *blk = alloc->freelist;
   void *result = NULL;
   if (blk != NULL) {
@@ -115,11 +144,13 @@ void* _ngf_blkalloc_alloc(_ngf_block_allocator *alloc) {
   return result;
 }
 
-_ngf_blkalloc_error _ngf_blkalloc_free(_ngf_block_allocator *alloc, void *ptr) {
+_ngf_blkalloc_error _ngf_blkalloc_free(_ngf_block_allocator *alloc,
+                                        void *ptr) {
   _ngf_blkalloc_error result = _NGF_BLK_NO_ERROR;
   if (ptr != NULL) {
     _ngf_blkalloc_block *blk = 
-        (_ngf_blkalloc_block*)((uint8_t*)ptr - offsetof(_ngf_blkalloc_block, data));
+        (_ngf_blkalloc_block*)((uint8_t*)ptr -
+                                offsetof(_ngf_blkalloc_block, data));
 
     uint32_t blk_tag = (~MARKER_MASK) & blk->header.marker_and_tag;
     uint32_t my_tag = alloc->tag;
