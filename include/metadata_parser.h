fdd787673533d1bde1e0b57f3892b59842f7c878
nicebyte@mailinator.com <nicebyte@gpfault.net>
2019-04-04 01:49:55 -0700

    [Metadata] Rename plmd -> ngf_plmd

--- include/metadata_parser.h
+++ include/metadata_parser.h
@@ -1,21 +1,24 @@
 /**
-Copyright © 2018 nicegraf contributors
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the “Software”), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ * Copyright (c) 2019 nicegraf contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
 #pragma once
 
@@ -31,7 +34,7 @@ SOFTWARE.
 extern "C" {
 #endif
 
-typedef struct plmd plmd;
+typedef struct plmd ngf_plmd;
 
 #define NGF_PLMD_DESC_UNIFORM_BUFFER         (0x00)
 #define NGF_PLMD_DESC_STORAGE_BUFFER         (0x01)
@@ -174,13 +177,13 @@ typedef struct ngf_plmd_alloc_callbacks {
 
 ngf_plmd_error ngf_plmd_load(const void *buf, size_t buf_size,
                              const ngf_plmd_alloc_callbacks *alloc_cb,
-                             plmd **result);
-void ngf_plmd_destroy(plmd *m, const ngf_plmd_alloc_callbacks *alloc_cb);
-const ngf_plmd_layout* ngf_plmd_get_layout(const plmd *m);
-const ngf_plmd_cis_map* ngf_plmd_get_image_to_cis_map(const plmd *m);
-const ngf_plmd_cis_map* ngf_plmd_get_sampler_to_cis_map(const plmd *m);
-const ngf_plmd_user* ngf_plmd_get_user(const plmd *m);
-const ngf_plmd_header* ngf_plmd_get_header(const plmd *m);
+                             ngf_plmd **result);
+void ngf_plmd_destroy(ngf_plmd *m, const ngf_plmd_alloc_callbacks *alloc_cb);
+const ngf_plmd_layout* ngf_plmd_get_layout(const ngf_plmd *m);
+const ngf_plmd_cis_map* ngf_plmd_get_image_to_cis_map(const ngf_plmd *m);
+const ngf_plmd_cis_map* ngf_plmd_get_sampler_to_cis_map(const ngf_plmd *m);
+const ngf_plmd_user* ngf_plmd_get_user(const ngf_plmd *m);
+const ngf_plmd_header* ngf_plmd_get_header(const ngf_plmd *m);
 
 const char* ngf_plmd_get_error_name(const ngf_plmd_error err);
 
