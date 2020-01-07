e324a4f42e09f7a63ffaf52cb6990d19b6f00197
nicebyte <nicebyte@gpfault.net>
2020-01-07 06:11:34 -0800

    [VK] remove logspam

--- source/nicegraf_impl_vk.c
+++ source/nicegraf_impl_vk.c
@@ -273,7 +273,6 @@ static VkShaderStageFlags get_vk_stage_flags(uint32_t flags) {
     result |= VK_SHADER_STAGE_FRAGMENT_BIT;
   if (flags & NGF_DESCRIPTOR_VERTEX_STAGE_BIT)
     result |= VK_SHADER_STAGE_VERTEX_BIT;
-  printf("vk stage flags %d\n", result);
   return result;
 }
 
@@ -2064,7 +2063,6 @@ ngf_error ngf_create_graphics_pipeline(const ngf_graphics_pipeline_info *info,
   _NGF_DARRAY_RESET(pipeline->vk_descriptor_set_layouts,
                     info->layout->ndescriptor_set_layouts);
   for (uint32_t s = 0u; s < info->layout->ndescriptor_set_layouts; ++s) {
-    printf("set %d\n", s);
     VkDescriptorSetLayoutBinding *vk_descriptor_bindings =
         NGF_ALLOCN(VkDescriptorSetLayoutBinding,
                    info->layout->descriptor_set_layouts[s].ndescriptors);
@@ -2074,7 +2072,6 @@ ngf_error ngf_create_graphics_pipeline(const ngf_graphics_pipeline_info *info,
       VkDescriptorSetLayoutBinding *vk_d = &vk_descriptor_bindings[b];
       const ngf_descriptor_info *d =
           &info->layout->descriptor_set_layouts[s].descriptors[b];
-      printf("binding %d %d %x\n", d->id, d->type, d->stage_flags);
       vk_d->binding         = d->id;
       vk_d->descriptorCount = 1u;
       vk_d->descriptorType  = get_vk_descriptor_type(d->type);
@@ -2103,7 +2100,6 @@ ngf_error ngf_create_graphics_pipeline(const ngf_graphics_pipeline_info *info,
   // Pipeline layout.
   const uint32_t ndescriptor_sets =
       _NGF_DARRAY_SIZE(pipeline->vk_descriptor_set_layouts);
-  printf("%d\n", ndescriptor_sets);
   const VkPipelineLayoutCreateInfo vk_pipeline_layout_info = {
     .sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
     .pNext                  = NULL,
