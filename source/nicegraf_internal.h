a573a6b5f227a5bcf7eb6c9e69fbbd95946edead
nicebyte <nicebyte@gpfault.net>
2020-06-22 19:20:05 -0700

    [internal] address empty variadic macro args problem

--- source/nicegraf_internal.h
+++ source/nicegraf_internal.h
@@ -183,9 +183,10 @@ typedef enum {
 #endif
 
 // Invoke diagnostic message callback directly.
+#define VA_ARGS(...) , ##__VA_ARGS__
 #define NGFI_DIAG_MSG(level, fmt, ...) \
 if (ngfi_diag_info.callback) { \
-  ngfi_diag_info.callback(level, ngfi_diag_info.userdata, fmt, __VA_ARGS__); \
+  ngfi_diag_info.callback(level, ngfi_diag_info.userdata, fmt VA_ARGS(__VA_ARGS__)); \
 }
 
 #ifdef __cplusplus
