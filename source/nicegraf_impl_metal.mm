d055d1bb90529e3487e36a930e6bd4062ea88b8e
Bagrat Dabaghyan <bagrat@triadastudio.com>
2020-11-07 23:59:33 +0400

    [vk,mtl,gl] add R16 signed and normalized image formats

--- source/nicegraf_impl_metal.mm
+++ source/nicegraf_impl_metal.mm
@@ -136,7 +136,10 @@ static mtl_format get_mtl_pixel_format(ngf_image_format f) {
     {MTLPixelFormatInvalid, 16, 16, 16, 0, 0, 0, false}, // RGB16F, unsupported on Metal
     {MTLPixelFormatRGBA16Float, 16, 16, 16, 16, 0, 0, false},
     {MTLPixelFormatRG11B10Float, 11, 11, 10, 0, 0, 0, false},
+    {MTLPixelFormatR16Unorm, 16, 0, 0, 0, 0, 0, false},
+    {MTLPixelFormatR16Snorm, 16, 0, 0, 0, 0, 0, false},
     {MTLPixelFormatR16Uint, 16, 0, 0, 0, 0, 0, false},
+    {MTLPixelFormatR16Sint, 16, 0, 0, 0, 0, 0, false},
     {MTLPixelFormatRG16Uint, 16, 16, 0, 0, 0, 0, false},
     {MTLPixelFormatInvalid, 16, 16, 16, 0, 0, 0, false}, // RGB16U, unsupported on Metal
     {MTLPixelFormatRGBA16Uint, 16, 16, 16, 16, 0, 0, false},
