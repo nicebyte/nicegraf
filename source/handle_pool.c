b05a32bb9eb6408036c904fc44344fea4aa562b3
nicebyte <nicebyte@gpfault.net>
2021-01-24 18:10:53 -0800

    [internal] handle pool initial version

---
+++ source/handle_pool.c
@@ -0,0 +1,74 @@
+#include "handle_pool.h"
+
+#include "dynamic_array.h"
+#include "nicegraf_internal.h"
+
+typedef struct ngfi_handle_pool_t {
+  NGFI_DARRAY_OF(uint64_t) handles;
+  pthread_mutex_t         lock;
+  ngfi_handle_pool_info   info;
+} ngfi_handle_pool_t;
+
+ngfi_handle_pool ngfi_create_handle_pool(const ngfi_handle_pool_info* info) {
+  bool             err    = false;
+  ngfi_handle_pool pool = NGFI_ALLOC(ngfi_handle_pool_t);
+
+  if (pool == NULL) {
+    err = true;
+    goto ngfi_handle_pool_cleanup;
+  }
+
+  pthread_mutex_init(&pool->lock, 0);
+
+  NGFI_DARRAY_RESET(pool->handles, info->initial_size);
+  pool->info = *info;
+  
+  for (size_t i = 0; i < pool->info.initial_size; ++i) {
+    const uint64_t handle = pool->info.allocator(pool->info.allocator_userinfo);
+    if (handle == 0u) {
+      goto ngfi_handle_pool_cleanup;
+    }
+    NGFI_DARRAY_APPEND(pool->handles, handle);
+  }
+
+ngfi_handle_pool_cleanup:
+  if (err) {
+    ngfi_destroy_handle_pool(pool);
+    return NULL;
+  } else {
+    return pool;
+  }
+}
+
+void ngfi_destroy_handle_pool(ngfi_handle_pool pool) {
+  if (pool != NULL) {
+    const size_t pool_size = NGFI_DARRAY_SIZE(pool->handles);
+    for(size_t i = 0; i < pool_size && pool->info.deallocator; ++i) {
+      pool->info.deallocator(NGFI_DARRAY_AT(pool->handles, i), pool->info.deallocator_userinfo);
+    }
+    NGFI_DARRAY_DESTROY(pool->handles);
+    pthread_mutex_destroy(&pool->lock);
+    NGFI_FREE(pool);
+  }
+}
+
+uint64_t ngfi_handle_pool_alloc(ngfi_handle_pool pool) {
+  assert(pool);
+  uint64_t result = 0u;
+  pthread_mutex_lock(&pool->lock);
+  const size_t pool_size = NGFI_DARRAY_SIZE(pool->handles);
+  if (pool_size > 0) {
+    result = *NGFI_DARRAY_BACKPTR(pool->handles);
+    NGFI_DARRAY_POP(pool->handles);
+  }
+  pthread_mutex_unlock(&pool->lock);
+  return result;
+}
+
+void ngfi_handle_pool_free(ngfi_handle_pool pool, uint64_t handle) {
+  assert(pool);
+  pthread_mutex_lock(&pool->lock);
+  NGFI_DARRAY_APPEND(pool->handles, handle);
+  pthread_mutex_unlock(&pool->lock);
+}
+
---
+++ source/handle_pool.h
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2021 nicegraf contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct ngfi_handle_pool_t* ngfi_handle_pool;
+typedef uint64_t (*ngfi_handle_allocator)(void*);
+typedef void (*ngfi_handle_deallocator)(uint64_t, void*);
+
+typedef struct {
+  uint32_t                initial_size;
+  ngfi_handle_allocator   allocator;
+  void*                   allocator_userinfo;
+  ngfi_handle_deallocator deallocator;
+  void*                   deallocator_userinfo;
+} ngfi_handle_pool_info;
+
+ngfi_handle_pool ngfi_create_handle_pool(const ngfi_handle_pool_info* info);
+void             ngfi_destroy_handle_pool(ngfi_handle_pool pool);
+uint64_t         ngfi_handle_pool_alloc(ngfi_handle_pool pool);
+void             ngfi_handle_pool_free(ngfi_handle_pool pool, uint64_t handle);
+
+#ifdef __cplusplus
+}
+#endif
