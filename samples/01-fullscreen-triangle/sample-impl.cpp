d9e06ffbdca0caff18d5c71133ffc09d548a5007
nicebyte <nicebyte@gpfault.net>
2021-04-03 09:26:50 -0700

    [wrappers] rename nicegraf_wrappers.h -> nicegraf-wrappers.h

--- samples/01-fullscreen-triangle/sample-impl.cpp
+++ samples/01-fullscreen-triangle/sample-impl.cpp
@@ -22,28 +22,50 @@
 
 #include "sample-interface.h"
 
+#include "nicegraf-wrappers.h"
 #include <stdio.h>
 
 namespace ngf_samples {
 
 struct sample_data {
-  uint32_t magic_number = 0xdeadbeef;
+  ngf::graphics_pipeline pipeline;
 };
 
 void* sample_initialize(uint32_t , uint32_t ) {
-  printf("sample initializing.\n");
   auto d = new sample_data{};
-  d->magic_number = 0xbadf00d;
-  printf("sample initialization complete.\n");
+  
   return static_cast<void*>(d);
 }
 
 void sample_draw_frame(
-    ngf_frame_token ,
+    ngf_frame_token token,
     uint32_t        ,
     uint32_t        ,
     float           ,
     void*           ) {
+    ngf_cmd_buffer cmdbuf = nullptr;
+    const ngf_cmd_buffer_info cmdbuf_info = {
+      .flags = 0u
+    };
+    ngf_create_cmd_buffer(&cmdbuf_info, &cmdbuf);
+    ngf_start_cmd_buffer(cmdbuf, token);
+    ngf_render_encoder renc;
+    ngf_cmd_buffer_start_render(cmdbuf, &renc);
+    constexpr ngf_attachment_load_op load_ops[2] = { NGF_LOAD_OP_DONTCARE, NGF_LOAD_OP_DONTCARE };
+    constexpr ngf_attachment_store_op store_ops[2] = { NGF_STORE_OP_DONTCARE, NGF_STORE_OP_DONTCARE }; 
+    ngf_render_target default_rt;
+    ngf_default_render_target(&default_rt);
+    ngf_pass_info pass = {
+      .render_target = default_rt,
+      .load_ops = load_ops,
+      .store_ops = store_ops,
+      .clears = nullptr,
+    };
+    ngf_cmd_begin_pass(renc, &pass);
+    ngf_cmd_end_pass(renc);
+    ngf_render_encoder_end(renc);
+    ngf_submit_cmd_buffers(1, &cmdbuf);
+    ngf_destroy_cmd_buffer(cmdbuf);
   //auto data = static_cast<sample_data*>(userdata);
   //printf("drawing frame %d (w %d h %d) at time %f magic number 0x%x\n", frame_token, width, height, time, data->magic_number);
 }
