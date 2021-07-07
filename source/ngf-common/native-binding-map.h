0867ff0e0fd73c17a6a719df47386bbdd1f62617
nicebyte <nicebyte@gpfault.net>
2021-07-06 22:29:38 -0700

    Revert "[common] rewrite native binding map"
    This reverts commit be244f0c18026b2190b8451d06ee827372019a19.

--- source/ngf-common/native-binding-map.h
+++ source/ngf-common/native-binding-map.h
@@ -23,28 +23,35 @@
 #pragma once
 
 #include "nicegraf.h"
-#include "ngf-common/pending-bind-op.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Maps (set, binding) pair to a platform's native binding id. */
-typedef struct ngfi_native_binding_map ngfi_native_binding_map;
+// Info about a native resource binding.
+typedef struct {
+  uint32_t ngf_binding_id;     // nicegraf binding id.
+  uint32_t native_binding_id;  // Actual backend api-specific binding id.
+  uint32_t ncis_bindings;      // Number of associated combined image/sampler
+                               // bindings.
+  uint32_t* cis_bindings;      // Associated combined image/sampler bindings.
+} ngfi_native_binding;
 
-/* Derives a (set, binding) => (native binding) mapping from a list of
-   bind operations. */
-ngfi_native_binding_map* ngfi_create_native_binding_map_from_pending_bind_ops(
-  const ngfi_pending_bind_op* pending_bind_ops_list);
+// Mapping from (set, binding) to (native binding).
+typedef ngfi_native_binding** ngfi_native_binding_map;
 
-/* Destroys a native binding map previously created with
-   ngfi_create_native_binding_map_... */
-void ngfi_destroy_native_binding_map(ngfi_native_binding_map* map);
+// Generates a (set, binding) to (native binding) map from the given pipeline
+// layout and combined image/sampler maps.
+ngf_error ngfi_create_native_binding_map(
+    const ngf_pipeline_layout_info* layout,
+    const ngf_plmd_cis_map*         images_to_cis,
+    const ngf_plmd_cis_map*         samplers_to_cis,
+    ngfi_native_binding_map*        result);
 
-/* Looks up a native binding from the given map using the provided nicegraf
-   set and binding ids. Returns 0xFFFF...FF on failure. */
-uint32_t ngfi_native_binding_map_lookup(
-  const ngfi_native_binding_map* map, uint32_t set, uint32_t binding);
+void ngfi_destroy_binding_map(ngfi_native_binding_map map);
+
+const ngfi_native_binding*
+ngfi_binding_map_lookup(const ngfi_native_binding_map map, uint32_t set, uint32_t binding);
 
 #ifdef __cplusplus
 }
