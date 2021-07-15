3c72b9080060bad8960272b7b7c6617171cff457
nicebyte <nicebyte@gpfault.net>
2021-07-14 23:33:59 -0700

    [common] rewrite native binding map AGAIN

--- source/ngf-common/native-binding-map.h
+++ source/ngf-common/native-binding-map.h
@@ -28,30 +28,29 @@
 extern "C" {
 #endif
 
-// Info about a native resource binding.
-typedef struct {
-  uint32_t ngf_binding_id;     // nicegraf binding id.
-  uint32_t native_binding_id;  // Actual backend api-specific binding id.
-  uint32_t ncis_bindings;      // Number of associated combined image/sampler
-                               // bindings.
-  uint32_t* cis_bindings;      // Associated combined image/sampler bindings.
-} ngfi_native_binding;
+/* Provides a mapping from nicegraf's (set, binding) to the backend platform's actual native binding. */
+typedef struct ngfi_native_binding_map ngfi_native_binding_map;
 
-// Mapping from (set, binding) to (native binding).
-typedef ngfi_native_binding** ngfi_native_binding_map;
+/**
+ * Finds the first instance of a serialized native binding map within a given character buffer and
+ * returns a pointer to it within the buffer. Returns NULL if not found.
+ */
+const char* ngfi_find_serialized_native_binding_map(const char* input);
 
-// Generates a (set, binding) to (native binding) map from the given pipeline
-// layout and combined image/sampler maps.
-ngf_error ngfi_create_native_binding_map(
-    const ngf_pipeline_layout_info* layout,
-    const ngf_plmd_cis_map*         images_to_cis,
-    const ngf_plmd_cis_map*         samplers_to_cis,
-    ngfi_native_binding_map*        result);
+/**
+ * Parses a native binding map out of the provided buffer. Returns NULL if parsing fails.
+ */
+ngfi_native_binding_map* ngfi_parse_serialized_native_binding_map(const char* serialized_native_binding_map);
 
-void ngfi_destroy_binding_map(ngfi_native_binding_map map);
+/**
+ * Looks up a binding from the given native binding map.
+ */
+uint32_t ngfi_native_binding_map_lookup(const ngfi_native_binding_map* map, uint32_t set, uint32_t binding);
 
-const ngfi_native_binding*
-ngfi_binding_map_lookup(const ngfi_native_binding_map map, uint32_t set, uint32_t binding);
+/**
+ * Deallocates any resources associated with the given native binding map.
+ */
+void ngfi_destroy_native_binding_map(ngfi_native_binding_map* map);
 
 #ifdef __cplusplus
 }
