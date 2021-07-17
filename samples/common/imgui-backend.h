92764742082f818ae1dfa28908363dbd8884a4d7
nicebyte <nicebyte@gpfault.net>
2021-07-16 19:41:20 -0700

    [samples] add imgui

---
+++ samples/common/imgui-backend.h
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2021 nicegraf contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "nicegraf.h"
+#include "nicegraf-wrappers.h"
+#include "imgui.h"
+
+namespace ngf_samples {
+
+/**
+ * This is a nicegraf-based rendering backend for ImGui.
+ * It's used to render the UI for samples.
+ */
+class ngf_imgui {
+  public:
+  /**
+   * Initializes the internal state of the ImGui rendering backend, and uploads
+   * the font texture by recording the appropriate commands into the given
+   * transfer encoder.
+   */
+  explicit ngf_imgui(ngf_xfer_encoder font_xfer_encoder);
+
+  /**
+   * Records commands for rendering the contents ofteh current ImGui draw data into the
+   * given render encoder.
+   */
+  void record_rendering_commands(ngf_render_encoder enc);
+
+  private:
+  struct uniform_data {
+    float ortho_projection[4][4];
+  };
+
+#if !defined(NGF_NO_IMGUI)
+  ngf::graphics_pipeline              pipeline_;
+  ngf::streamed_uniform<uniform_data> uniform_data_;
+  ngf::image                          font_texture_;
+  ngf::sampler                        tex_sampler_;
+  ngf::attrib_buffer                  attrib_buffer_;
+  ngf::index_buffer                   index_buffer_;
+  ngf::pixel_buffer                   texture_data_;
+  ngf::shader_stage                   vertex_stage_;
+  ngf::shader_stage                   fragment_stage_;
+  ngf::render_target                  default_rt_;
+#endif
+};
+
+}
+
+
+
