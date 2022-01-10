ded6f46d047198b4419aa1c99b805f00a5a2d221
nicebyte <nicebyte@gpfault.net>
2022-01-09 20:51:12 -0800

    [samples] use ConstantBuffer in polygon.hlsl

--- samples/shaders/polygon.hlsl
+++ samples/shaders/polygon.hlsl
@@ -3,13 +3,16 @@
 #define GENERIC_FS_INPUT_HAS_COLOR
 #include "generic-frag-shader-input.hlsl"
 
-[[vk::binding(0, 0)]] cbuffer Uniforms {
-  float u_ScaleA;
-  float u_ScaleB;
-  float u_Time;
-  float u_AspectRatio;
-  float u_Theta;
+struct VertShaderUniforms {
+  float scaleA;
+  float scaleB;
+  float time;
+  float aspectRatio;
+  float theta;
 };
+
+
+[[vk::binding(0, 0)]] ConstantBuffer<VertShaderUniforms> vertShaderUniforms;
 
 float4 PSMain(GenericFragShaderInput vertexAttribs) : SV_Target {
   return vertexAttribs.color;
@@ -21,17 +24,18 @@ GenericFragShaderInput VSMain(uint vertexId : SV_VertexID) {
     polygonVertexData.position = float4(0.0, 0.0, 0.0, 1.0);
     polygonVertexData.color = float4(0.8, 0.7, 0.8, 1.0);
   } else {
-    float    rotationAngle = u_Time;
+    float    rotationAngle = vertShaderUniforms.time;
     float2x2 rotationMatrix = {
       cos(rotationAngle), -sin(rotationAngle), 
       sin(rotationAngle), cos(rotationAngle)
     };
-    float effectiveScale = (vertexId % 2  ? u_ScaleB : u_ScaleA);
+    float effectiveScale = (vertexId % 2  ? vertShaderUniforms.scaleB : vertShaderUniforms.scaleA);
     int outerVertexId = int(round(float(vertexId)/3.0));
+    float theta = vertShaderUniforms.theta;
     float2 vertexPosition = mul(rotationMatrix,
-                                float2(sin(outerVertexId * u_Theta),
-                                       cos(outerVertexId * u_Theta))) * float2(1.0, u_AspectRatio)
-                                                                      * effectiveScale;
+                                float2(sin(outerVertexId * theta),
+                                       cos(outerVertexId * theta))) * float2(1.0, vertShaderUniforms.aspectRatio)
+                                                                    * effectiveScale;
     polygonVertexData.position = float4(vertexPosition, 0.0, 1.0);
     polygonVertexData.color = float4(0.5 * (vertexPosition.x + 1.0), 0.5 * (vertexPosition.y + 1.0), abs(1.0 - vertexPosition.x), 1.0);
   }
