fae11824e5cb46d6ce7b1bfab2607df5927fd594
nicebyte <nicebyte@gpfault.net>
2022-01-11 22:54:07 -0800

    [samples] prepare the cubemap shader for cubemap arrays

--- samples/shaders/cubemap.hlsl
+++ samples/shaders/cubemap.hlsl
@@ -1,15 +1,24 @@
-// T: cubemap vs:VSMain ps:PSMain
+// T: cubemap vs:VSMain ps:PSMain define:GENERIC_FS_INPUT_HAS_CLIPSPACE_POS=1
+// T: cubemap-array vs:VSMain ps:PSMain define:GENERIC_FS_INPUT_HAS_CLIPSPACE_POS=1 define:USE_CUBEMAP_ARRAY=1
 
-#define GENERIC_FS_INPUT_HAS_CLIPSPACE_POS
 #include "triangle.hlsl"
 
 struct ShaderUniforms {
   float4x4 cameraTransform;
   float    aspectRatio;
+#if defined(USE_CUBEMAP_ARRAY)
+  float    cubemapArrayIndex;
+#endif
 };
 
+#if defined(USE_CUBEMAP_ARRAY)
+#define TEXTURE_IMAGE_TYPE TextureCubeArray
+#else
+#define TEXTURE_IMAGE_TYPE TextureCube
+#endif
+
 [[vk::binding(0, 0)]] ConstantBuffer<ShaderUniforms> shaderUniforms;
-[[vk::binding(1, 0)]] uniform TextureCube cubemapImage;
+[[vk::binding(1, 0)]] uniform TEXTURE_IMAGE_TYPE cubemapImage;
 [[vk::binding(2, 0)]] uniform sampler imageSampler;
 
 float4 PSMain(GenericFragShaderInput vertexAttribs) : SV_Target {
@@ -18,7 +27,12 @@ float4 PSMain(GenericFragShaderInput vertexAttribs) : SV_Target {
                                  vertexAttribs.clipSpacePosition.y,
                                 -1.0,
                                  0.0)).xyz;
-  return cubemapImage.Sample(imageSampler, direction);
+#if defined(USE_CUBEMAP_ARRAY)
+  float4 cubemapSampleCoords = float4(direction, shaderUniforms.cubemapArrayIndex);
+#else
+  float3 cubemapSampleCoords = direction;
+#endif
+  return cubemapImage.Sample(imageSampler, cubemapSampleCoords);
 }
 
 GenericFragShaderInput VSMain(uint vertexId : SV_VertexID) {
