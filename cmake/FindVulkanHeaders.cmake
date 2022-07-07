f64dab41976e3256a67b70059d07e561114fbc01
Andranik Melikyan <andranikmelikyan@gmail.com>
2022-07-08 00:20:01 +0400

    [build] fix FindVulkanHeaders failing to find package if VulkanHeaders was found beforehand

--- cmake/FindVulkanHeaders.cmake
+++ cmake/FindVulkanHeaders.cmake
@@ -1,29 +1,32 @@
 # Probe command-line arguments and the environment to see if they specify the
 # Vulkan headers installation path.
-if(NOT DEFINED VULKAN_HEADERS_INSTALL_DIR)
-  if (DEFINED ENV{VULKAN_HEADERS_INSTALL_DIR})  
-    set(VULKAN_HEADERS_INSTALL_DIR "$ENV{VULKAN_HEADERS_INSTALL_DIR}")
-  elseif(DEFINED ENV{VULKAN_SDK})
-    set(VULKAN_HEADERS_INSTALL_DIR "$ENV{VULKAN_SDK}")
+if(NOT DEFINED VulkanHeaders_INCLUDE_DIR)
+  if(NOT DEFINED VULKAN_HEADERS_INSTALL_DIR)
+    if (DEFINED ENV{VULKAN_HEADERS_INSTALL_DIR})
+      set(VULKAN_HEADERS_INSTALL_DIR "$ENV{VULKAN_HEADERS_INSTALL_DIR}")
+    elseif(DEFINED ENV{VULKAN_SDK})
+      set(VULKAN_HEADERS_INSTALL_DIR "$ENV{VULKAN_SDK}")
+    endif()
   endif()
-endif()
 
-if(DEFINED VULKAN_HEADERS_INSTALL_DIR)
-  # When CMAKE_FIND_ROOT_PATH_INCLUDE is set to ONLY, the HINTS in find_path()
-  # are re-rooted, which prevents VULKAN_HEADERS_INSTALL_DIR to work as
-  # expected. So use NO_CMAKE_FIND_ROOT_PATH to avoid it.
+  if(DEFINED VULKAN_HEADERS_INSTALL_DIR)
+    # When CMAKE_FIND_ROOT_PATH_INCLUDE is set to ONLY, the HINTS in find_path()
+    # are re-rooted, which prevents VULKAN_HEADERS_INSTALL_DIR to work as
+    # expected. So use NO_CMAKE_FIND_ROOT_PATH to avoid it.
 
-  # Use HINTS instead of PATH to search these locations before
-  # searching system environment variables like $PATH that may
-  # contain SDK directories.
-  find_path(VulkanHeaders_INCLUDE_DIR
-      NAMES vulkan/vulkan.h
-      HINTS ${VULKAN_HEADERS_INSTALL_DIR}/include
-      NO_CMAKE_FIND_ROOT_PATH)
-else()
-  # If VULKAN_HEADERS_INSTALL_DIR, or one of its variants was not specified,
-  # do a normal search without hints.
-  find_path(VulkanHeaders_INCLUDE_DIR NAMES vulkan/vulkan.h HINTS "${CMAKE_CURRENT_SOURCE_DIR}/external/Vulkan-Headers/include" NO_CMAKE_FIND_ROOT_PATH)
+    # Use HINTS instead of PATH to search these locations before
+    # searching system environment variables like $PATH that may
+    # contain SDK directories.
+
+    find_path(VulkanHeaders_INCLUDE_DIR
+        NAMES vulkan/vulkan.h
+        HINTS ${VULKAN_HEADERS_INSTALL_DIR}/include
+        NO_CMAKE_FIND_ROOT_PATH)
+  else()
+    # If VULKAN_HEADERS_INSTALL_DIR, or one of its variants was not specified,
+    # do a normal search without hints.
+    find_path(VulkanHeaders_INCLUDE_DIR NAMES vulkan/vulkan.h HINTS "${CMAKE_CURRENT_SOURCE_DIR}/external/Vulkan-Headers/include" NO_CMAKE_FIND_ROOT_PATH)
+  endif()
 endif()
 
 set(VulkanHeaders_INCLUDE_DIRS ${VulkanHeaders_INCLUDE_DIR})
