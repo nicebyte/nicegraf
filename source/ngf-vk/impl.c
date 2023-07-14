a27cdcdce17d1e270dc25eba93d6401b65e429bf
Bryson Miller <andrewmiller77@protonmail.com>
2023-07-14 14:20:26 -0700

    removing merge artifacts

--- source/ngf-vk/impl.c
+++ source/ngf-vk/impl.c
@@ -2724,7 +2724,6 @@ ngf_enumerate_devices_cleanup:
 ngf_error ngf_initialize(const ngf_init_info* init_info) {
   assert(init_info);
 
-<<<<<<< HEAD
   if (init_info->renderdoc_info) {
     ModuleHandle ngf_renderdoc_mod = LoadLibraryA(init_info->renderdoc_info->renderdoc_lib_path);
     if (ngf_renderdoc_mod != NULL) {
@@ -2733,20 +2732,14 @@ ngf_error ngf_initialize(const ngf_init_info* init_info) {
       if (!RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_6_0, (void**)&_renderdoc.api)) {
         return NGF_ERROR_OBJECT_CREATION_FAILED;
       }
-<<<<<<< HEAD
       if (init_info->renderdoc_info->renderdoc_destination_template) {
         _renderdoc.api->SetCaptureFilePathTemplate(
             init_info->renderdoc_info->renderdoc_destination_template);
       }
-=======
-      _renderdoc.api->SetCaptureFilePathTemplate(init_info->renderdoc_info->renderdoc_destination_template);
->>>>>>> 13bb5278086cbdb9295b9adcd4e1640babc7e147
       _renderdoc.is_capturing_next_frame = false;
     }
   }
 
-||||||| 66db87f
-=======
   if (init_info->renderdoc_info) {
     ModuleHandle ngf_renderdoc_mod = LoadLibraryA(init_info->renderdoc_info->renderdoc_lib_path);
     if (ngf_renderdoc_mod != NULL) {
@@ -2760,7 +2753,6 @@ ngf_error ngf_initialize(const ngf_init_info* init_info) {
     }
   }
 
->>>>>>> 13bb5278086cbdb9295b9adcd4e1640babc7e147
   // Sanity checks.
   if (!init_info) { return NGF_ERROR_INVALID_OPERATION; }
   if (_vk.instance != VK_NULL_HANDLE) {
