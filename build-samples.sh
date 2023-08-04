eae44ae3e367488d708adef382b5e30aa08d95d0
nicebyte <nicebyte@gpfault.net>
2023-08-03 23:33:40 -0700

    [build] update unix samples build script

--- build-samples.sh
+++ build-samples.sh
@@ -2,15 +2,21 @@
 
 set -e
 
-echo "Downloading binary dependencies for samples"
-git lfs install
-git lfs pull --exclude=""
-echo "Downloading library dependencies for samples"
+echo "Downloading binary dependencies for samples..."
+curl https://github.com/nicebyte/nicegraf/releases/download/v0.1/nicegraf-samples-data.zip -fL -o nicegraf-samples-data.zip
+echo "Unpacking binary dependencies and data for samples..."
+unzip -u nicegraf-samples-data.zip
+chmod +x ./samples/deps/niceshade/macos/niceshade
+chmod +x ./samples/deps/niceshade/linux/niceshade
+echo "Removing temporary files..."
+rm -rf nicegraf-samples-data.zip
+echo "Downloading library dependencies for samples..."
 git submodule init
 git submodule update
+echo "Setting up folder for build files..."
 mkdir -p samples-build-files
 cd samples-build-files
-
+echo "Generating build files..."
 if  [ "`uname -s`" = "Darwin" ]; then
   NGF_GENERATOR="-GXcode" 
 else
@@ -19,3 +25,4 @@ fi
 
 cmake .. -DNGF_BUILD_SAMPLES="yes" ${NGF_GENERATOR} $@
 cd ..
+echo "Finished successfully!"
