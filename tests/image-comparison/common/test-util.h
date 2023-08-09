0ee3d75cda28aaf523cce110761ddfceb16de776
Aditya Choubey <62399750+aditya-c2512@users.noreply.github.com>
2023-08-09 11:52:07 +0530

    Image comparison unit testing skeleton code (#176)

---
+++ tests/image-comparison/common/test-util.h
@@ -0,0 +1,142 @@
+#include "nicegraf.h"
+#include "nicegraf-wrappers.h"
+#include "nicegraf-util.h"
+#include "check.h"
+
+struct test_info{
+  ngf::cmd_buffer main_cmd_buffer;
+  ngf_frame_token frame_token;
+};
+
+struct test_info ngf_test_init()
+{
+  uint32_t          ndevices = 0u;
+  const ngf_device* devices  = NULL;
+  ngf_get_device_list(&devices, &ndevices);
+  const char* device_perf_tier_names[NGF_DEVICE_PERFORMANCE_TIER_COUNT] = {
+      "high",
+      "low",
+      "unknown"};
+
+  size_t high_power_device_idx = (~0u);
+  for (uint32_t i = 0; i < ndevices; ++i) {
+    if (high_power_device_idx == (~0u) &&
+        devices[i].performance_tier == NGF_DEVICE_PERFORMANCE_TIER_HIGH) {
+      high_power_device_idx = i;
+    }
+  }
+  /* Fall back to 1st device if no high-power device was found. */
+  const size_t preferred_device_idx = (high_power_device_idx == ~0u) ? 0 : high_power_device_idx;
+  const ngf_device_handle device_handle = devices[preferred_device_idx].handle;
+
+  const ngf_init_info init_info {
+      .diag_info            = &diagnostic_info,
+      .allocation_callbacks = NULL,
+      .device               = device_handle,
+      .renderdoc_info       = NULL};
+  ngf_initialize(&init_info);
+
+  const ngf_sample_count main_render_target_sample_count = ngf_get_device_capabilities()->max_supported_framebuffer_color_sample_count;
+  const ngf_swapchain_info swapchain_info                  = {
+                       .color_format  = NGF_IMAGE_FORMAT_BGRA8_SRGB,
+                       .depth_format  = NGF_IMAGE_FORMAT_DEPTH32,
+                       .sample_count  = main_render_target_sample_count,
+                       .capacity_hint = 3u,
+                       .width         = (uint32_t)fb_width,
+                       .height        = (uint32_t)fb_height,
+                       .native_handle = native_window_handle,
+                       .present_mode  = NGF_PRESENTATION_MODE_FIFO};
+  const ngf_context_info ctx_info = {.swapchain_info = &swapchain_info, .shared_context = nullptr};
+  ngf::context           context;
+
+  context.initialize(ctx_info);
+  ngf_set_context(context);
+
+  ngf::cmd_buffer main_cmd_buffer;
+  main_cmd_buffer.initialize(ngf_cmd_buffer_info {});
+  ngf_frame_token frame_token;
+  if (ngf_begin_frame(&frame_token) == NGF_ERROR_OK) return;
+  ngf_start_cmd_buffer(main_cmd_buffer, frame_token);
+  
+  struct test_info info = {
+    .main_cmd_buffer = main_cmd_buffer,
+    .frame_token = frame_token
+  };
+  return info;
+}
+
+void ngf_test_shutdown(struct test_info& info){
+  // submit main cmd buffer
+  ngf_cmd_buffer submitted_cmd_bufs[] = {info.main_cmd_buffer.get()};
+  ngf_submit_cmd_buffers(1, submitted_cmd_bufs);
+  // end frame
+  ngf_end_frame(info.frame_token);
+  // ngf_shutdown()
+  ngf_shutdown();
+}
+
+bool ngf_validate_result(ngf_image output_image, const char* ref_image, ngf_frame_token frame_token)
+{
+  ngf_cmd_buffer      xfer_cmd_buf  = nullptr;
+  ngf_cmd_buffer_info xfer_cmd_info = {};
+
+  ngf_extent3d        src_extent = {512u, 512u, 1u};
+  ngf_offset3d        src_offset = {0u, 0u, 0u};
+  const ngf_image_ref src_image  = {
+       .image        = output_image.get(),
+       .mip_level    = 0u,
+       .layer        = 0u,
+       .cubemap_face = NGF_CUBEMAP_FACE_COUNT
+  };
+  ngf_buffer      dst_buffer;
+  ngf_buffer_info dst_buffer_info = {
+      .size         = 512u * 512u * 4u,
+      .storage_type = NGF_BUFFER_STORAGE_HOST_READABLE,
+      .buffer_usage = NGF_BUFFER_USAGE_XFER_DST
+  };
+  ngf_create_buffer(&dst_buffer_info, &dst_buffer);
+  ngf_create_cmd_buffer(&xfer_cmd_info, &xfer_cmd_buf);
+  ngf_start_cmd_buffer(xfer_cmd_buf, frame_token);
+  ngf_xfer_encoder   xfer_encoder {};
+  ngf_xfer_pass_info xfer_pass_info {};
+  NGF_SAMPLES_CHECK_NGF_ERROR(ngf_cmd_begin_xfer_pass(xfer_cmd_buf, &xfer_pass_info, &xfer_encoder));
+  ngf_cmd_copy_image_to_buffer(xfer_encoder, src_image, src_offset, src_extent, 1u, dst_buffer, 0u);
+  NGF_SAMPLES_CHECK_NGF_ERROR(ngf_cmd_end_xfer_pass(xfer_encoder));
+  ngf_submit_cmd_buffers(1, &xfer_cmd_buf);
+  ngf_destroy_cmd_buffer(xfer_cmd_buf);
+  ngf_finish();
+
+  void* host_dst = ngf_buffer_map_range(dst_buffer, 0u, 512u * 512u * 4u);
+
+  // compare host_dst to references/ref_image
+  size_t buff_size = 512u*512u*4u;
+  FILE* file = fopen(ref_image, "rb");
+
+  if(file == NULL) return false;
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  if (file_size != buff_size) {
+    fclose(file);
+    return false;
+  }
+
+  fseek(file, 0, SEEK_SET);
+  char* buffer = (char*)malloc(buff_size);
+  fread(buffer, 1, buff_size, file);
+  fclose(file);
+
+  bool equal = true;
+  const char* mem_data = (const char*)host_dst;
+  for (size_t i = 0; i < buff_size; i++) {
+      if (mem_data[i] != buffer[i]) {  
+          free(buffer);
+          equal = false;
+      }
+  }
+  if(!equal){
+    FILE* f = fopen("./output/triangle_test.data", "wb");
+    fwrite(host_dst, 1u, 512 * 512 * 4, f);
+    fclose(f);
+  }
+  return equal;
+}
---
+++ tests/image-comparison/unit-tests/ngf-triangle-test.cpp
@@ -0,0 +1,103 @@
+#include "nicegraf.h"
+#include "test-util.h"
+#include "nicegraf-wrappers.h"
+#include "nicegraf-util.h"
+#include "shader-loader.h"
+#include "check.h"
+
+#include <stdio.h>
+
+#if defined(__APPLE__)
+#define NGF_TESTS_COMMON_MAIN apple_main
+#else
+#define NGF_TESTS_COMMON_MAIN main
+#endif
+
+void ngf_test_draw(ngf_image output_image, ngf_frame_token frame_token){
+  // Initialize the triangle test
+  ngf::render_target offscreen_rt;
+  ngf::graphics_pipeline offscreen_pipeline;
+
+  const ngf_attachment_description offscreen_color_attachment_description {
+    .type = NGF_ATTACHMENT_COLOR,
+    .format = NGF_IMAGE_FORMAT_BGRA8_SRGB,
+    .sample_count = NGF_SAMPLE_COUNT_1,
+    .is_sampled = true
+  };
+  const ngf_attachment_descriptions attachments_list = {
+    .descs = &offscreen_color_attachment_description,
+    .ndescs = 1u,
+  };
+  const ngf_image_ref img_ref = {
+    .image = output_image.get(),
+    .mip_level = 0u,
+    .layer = 0u,
+    .cubemap_face = NGF_CUBEMAP_FACE_COUNT
+  };
+  ngf_render_target_info rt_info {
+    &attachments_list,
+    &img_ref
+  };
+
+  offscreen_rt.initialize(rt_info);
+  const ngf::shader_stage offscreen_vertex_stage = ngf_samples::load_shader_stage("small-triangle", "VSMain", NGF_STAGE_VERTEX);
+  const ngf::shader_stage offscreen_fragment_stage = ngf_samples::load_shader_stage("small-triangle", "PSMain", NGF_STAGE_FRAGMENT);
+
+  ngf_util_graphics_pipeline_data offscreen_pipeline_data;
+  ngf_util_create_default_graphics_pipeline_data(&offscreen_pipeline_data);
+  ngf_graphics_pipeline_info &offscreen_pipe_info =
+      offscreen_pipeline_data.pipeline_info;
+  offscreen_pipe_info.nshader_stages = 2u;
+  offscreen_pipe_info.shader_stages[0] = offscreen_vertex_stage.get();
+  offscreen_pipe_info.shader_stages[1] = offscreen_fragment_stage.get();
+  offscreen_pipe_info.compatible_rt_attachment_descs = &attachments_list;
+  offscreen_pipeline.initialize(offscreen_pipe_info);
+
+  // Start drawing to output_image
+  ngf_irect2d         offsc_viewport {0, 0, 512, 512};
+  ngf_cmd_buffer      offscr_cmd_buf = nullptr;
+  ngf_cmd_buffer_info cmd_info       = {};
+  ngf_create_cmd_buffer(&cmd_info, &offscr_cmd_buf);
+  ngf_start_cmd_buffer(offscr_cmd_buf, frame_token);
+  {
+    ngf::render_encoder renc {offscr_cmd_buf, offscreen_rt, .0f, 0.0f, 0.0f, 0.0f, 1.0, 0u};
+    ngf_cmd_bind_gfx_pipeline(renc, offscreen_pipeline);
+    ngf_cmd_viewport(renc, &offsc_viewport);
+    ngf_cmd_scissor(renc, &offsc_viewport);
+    ngf_cmd_draw(renc, false, 0u, 3u, 1u);
+  }
+  ngf_submit_cmd_buffers(1, &offscr_cmd_buf);
+  ngf_destroy_cmd_buffer(offscr_cmd_buf);
+}
+
+int NGF_SAMPLES_COMMON_MAIN(int, char**){
+  // ngf_test_init(...): initializes nicegraf; common for all tests
+  struct test_info info = ngf_test_init();
+
+  // [PENDING] Create an ngf_image to render to
+  ngf::image output_image;
+  const ngf_extent3d img_size { 512u, 512u, 1u };
+  const ngf_image_info img_info {
+    NGF_IMAGE_TYPE_IMAGE_2D,
+    img_size,
+    1u,
+    1u,
+    NGF_IMAGE_FORMAT_BGRA8_SRGB,
+    NGF_SAMPLE_COUNT_1,
+    NGF_IMAGE_USAGE_SAMPLE_FROM | NGF_IMAGE_USAGE_ATTACHMENT
+  };
+  output_image.initialize(img_info);
+
+  // ngf_test_draw(...): initializes test and draws the test render to output_image
+  ngf_test_draw(output_image, info.frame_token);
+
+  // ngf_validate_result(ngf_image, const char*): if false, save the output_image to log the issue. if true, test is passed
+  if(!ngf_validate_result(output_image, "references/triangle_reference.data", info.frame_token))
+  {
+    // Print Test failure message
+    printf("[NICEGRAF TEST] Test failed. Please view output image in the output directory.\n");
+  }
+
+  // Shutdown test
+  ngf_test_shutdown(info);
+}
