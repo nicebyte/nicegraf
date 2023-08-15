39e22a24e22cc57dbe6bb37b79d20548e199cfc8
Andrew Miller <andrewmiller77@protonmail.com>
2023-08-14 22:43:07 -0700

    image-comparsion CMakeLists.txt

---
+++ tests/image-comparison/common/check.h
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2021 nicegraf contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "logging.h"
+#include "nicegraf-util.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma warning(disable : 26812)
+
+namespace ngf_image_comparison {
+
+#define NGF_IMAGE_COMPARISON_CHECK_NGF_ERROR(expr)            \
+  {                                                           \
+    const ngf_error err = (expr);                             \
+    if (err != NGF_ERROR_OK) {                                \
+      ::ngf_image_comparison::loge(                           \
+          "nicegraf error %d (file %s line %d), aborting.\n", \
+          err,                                                \
+          __FILE__,                                           \
+          __LINE__);                                          \
+      fflush(stderr);                                         \
+      abort();                                                \
+    }                                                         \
+  }
+
+#define NGF_IMAGE_COMPARISON_ASSERT(expr)            \
+  {                                                  \
+    if (!(expr)) {                                   \
+      ::ngf_image_comparison::loge(                  \
+          "assertion %s failed (file %s line %d)\n", \
+          #expr,                                     \
+          __FILE__,                                  \
+          __LINE__);                                 \
+      fflush(stderr);                                \
+      abort();                                       \
+    }                                                \
+  }
+
+}  // namespace ngf_image_comparison
