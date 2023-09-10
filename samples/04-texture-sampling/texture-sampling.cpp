aa65bea09ceaffd0f111004804a60bb29a9acdda
nicebyte <nicebyte@gpfault.net>
2023-09-10 13:13:34 -0700

    [samples] remove commented-out code

--- samples/04-texture-sampling/texture-sampling.cpp
+++ samples/04-texture-sampling/texture-sampling.cpp
@@ -99,18 +99,7 @@ void* sample_initialize(
       s->texture.get(),
       &img_write,
       1u);
-  /*ngf_cmd_write_image(
-      xfer_encoder,
-      texture_staging_image.staging_buffer.get(),
-      0u,
-      ngf_image_ref {
-          .image        = s->texture.get(),
-          .mip_level    = 0u,
-          .layer        = 0u,
-          .cubemap_face = NGF_CUBEMAP_FACE_COUNT},
-      ngf_offset3d {},
-      ngf_extent3d {texture_staging_image.width_px, texture_staging_image.height_px, 1u},
-      1u);*/
+
 
   /* Populate the rest of the mip levels automatically. */
   ngf_cmd_generate_mipmaps(xfer_encoder, s->texture.get());
