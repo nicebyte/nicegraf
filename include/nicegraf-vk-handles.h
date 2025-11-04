46647854745ea8852a78c05cba9c614459f51426
Bagrat Dabaghyan <dbagrat@gmail.com>
2025-11-04 04:02:15 +0400

    fix comments

--- include/nicegraf-vk-handles.h
+++ include/nicegraf-vk-handles.h
@@ -30,16 +30,16 @@ extern "C" {
 /**
  * \ingroup ngf
  *
- * Returns a uintptr_t to the underlying VkDevice. The caller is responsible for casting the return
- * value to a VkDevice.
+ * Returns the underlying VkDevice handle cast to uintptr_t. The caller is responsible for casting
+ * the return value to a VkDevice.
  */
 uintptr_t ngf_get_vk_device_handle() NGF_NOEXCEPT;
 
 /**
  * \ingroup ngf
  *
- * Returns a uintptr_t to the underlying VkInstance. The caller is responsible for casting the return
- * value to a VkInstance.
+ * Returns the underlying VkInstance handle cast to uintptr_t. The caller is responsible for casting
+ * the return value to a VkInstance.
  */
 uintptr_t ngf_get_vk_instance_handle() NGF_NOEXCEPT;
 
